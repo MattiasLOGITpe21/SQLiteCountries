551c632ab04e9710691fdb7d60fa85782cc4fd99
Opilane TTHK <opilane@tthk.ee>
2022-02-18 10:57:42 +0200

    SQLite Countries

---
+++ SQLiteCountries/Database.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace SQLiteCountries
+{
+    class Database
+    {
+        public SQLiteConnection myConnection;
+
+        public Database()
+        {
+            myConnection = new SQLiteConnection("Data Source=sqlite_madis.db");
+
+
+        }
+
+        public void OpenConnection()
+        {
+            if(myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Closed)
+            {
+                myConnection.Close();
+            }
+        }
+    }
+}
---
+++ SQLiteCountries/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace SQLiteCountries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadCountries();
+        }
+
+        private static void ReadCountries()
+        {
+            Database databaseObj = new Database();
+
+            string query = "SELECT Country.countryName AS Country, Capital.capitalname As Capital FROM Capital JOIN Country ON Capital.countryID = Country.rowid";
+            SQLiteCommand myCommand = new SQLiteCommand(query, databaseObj.myConnection);
+            databaseObj.OpenConnection();
+
+            SQLiteDataReader data = myCommand.ExecuteReader();
+
+            if(data.HasRows)
+            {
+                while(data.Read())
+                {
+                    Console.WriteLine($"Country: {data["Country"]}. Capital: {data["Capital"]}");
+
+                }
+            }
+
+            databaseObj.CloseConnection();
+        }
+    }
+}
